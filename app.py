6995b8fcff747505271d1fd435b89d3e38b26426
yurets <yesterrrday@gmail.com>
2025-08-12 13:07:28 -0700

    yo2

--- app.py
+++ app.py
@@ -2,23 +2,16 @@ import pandas as pd
 import plotly.express as px
 from dash import Dash, html, dcc, dash_table
 
-# Load data
 url = "https://raw.githubusercontent.com/half-man-half-potato/cv/master/data.csv"
 df = pd.read_csv(url)
 
-# Create df_table
 df_table = df[['Client_ID', 'Employer', 'Country', 'Client_Name_Full', 'Project']].drop_duplicates()
 df_table = df_table.sort_values(by='Client_ID').drop(columns=['Client_ID'])
 
-# Create df_gantt
-df_gantt = df[['Client_ID', 'Client_Name_Full', 'Start_Date', 'End_Date', 'Duration', 'Employer']].drop_duplicates()
-df_gantt = df_gantt.sort_values(by='Client_ID', ascending=False)
-
-# Convert date columns for Gantt chart
+df_gantt = df[['Client_ID', 'Client_Name_Full', 'Start_Date', 'End_Date', 'Employer']].drop_duplicates()
 df_gantt['Start_Date'] = pd.to_datetime(df_gantt['Start_Date'])
 df_gantt['End_Date'] = pd.to_datetime(df_gantt['End_Date'])
 
-# Create Gantt chart
 fig_gantt = px.timeline(
     df_gantt,
     x_start="Start_Date",
@@ -27,7 +20,6 @@ fig_gantt = px.timeline(
     color="Employer"
 )
 
-# Hide Y axis, legend, and title
 fig_gantt.update_layout(
     showlegend=False,
     yaxis=dict(visible=False),
@@ -35,11 +27,9 @@ fig_gantt.update_layout(
     margin=dict(l=0, r=0, t=0, b=0)
 )
 
-# Dash app
 app = Dash(__name__)
 
 app.layout = html.Div([
-    # Gantt chart div
     html.Div(
         dcc.Graph(figure=fig_gantt, config={"displayModeBar": False}),
         style={
@@ -50,7 +40,6 @@ app.layout = html.Div([
             "height": "600px"
         }
     ),
-    # Table div
     html.Div(
         dash_table.DataTable(
             columns=[{"name": col, "id": col} for col in df_table.columns],
